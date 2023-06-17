9f211d3b92b519b769f78b26e43bb07b388dafc2
Robson Matos <nosbor_album@hotmail.com>
2023-06-16 23:12:12 -0300

    Commit

--- script.js
+++ script.js
@@ -97,4 +97,5 @@ function loadItens() {
 const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
 const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
 
+
 loadItens()
